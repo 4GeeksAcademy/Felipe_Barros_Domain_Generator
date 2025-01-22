15429385409900af9ff50d4487966dcf05c50085
PipeBarros <felipebarroy@gmail.com>
2025-01-22 13:02:26 +0000

    FelipeB_Domain_Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ccTLD = [".es", ".com", ".cl"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainArrays = [];
+  pronoun.forEach(pronouns => {
+    adj.forEach(adjs => {
+      noun.forEach(nouns => {
+        ccTLD.forEach(ccTLDs =>
+          domainArrays.push(pronouns + adjs + nouns + ccTLDs)
+        );
+      });
+    });
+  });
+
+  function createList(element) {
+    return `<li>${element}</li>`;
+  }
+
+  function createHtmlList(arr) {
+    let lisOk = arr.map(createList);
+
+    let htmlUl = "<ul>";
+    for (let lis of lisOk) {
+      htmlUl += lis;
+    }
+    htmlUl += "</ul>";
+    return htmlUl;
+  }
+
+  document.getElementById("domainGenerator").innerHTML = createHtmlList(
+    domainArrays
+  );
 };
